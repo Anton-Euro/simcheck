445fe8e2eaa48477a445ee260fb98c1d1ede91e3
Anton-Euro <silikonov15@mail.ru>
2020-07-17 22:40:04 +0300

    first build

---
+++ bot.py
@@ -0,0 +1,24 @@
+import config
+import telebot
+import requests
+import json
+import time
+
+bot = telebot.TeleBot(config.TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+	bot.send_message(message.chat.id, "Поиск начался")
+	google_last = 0
+	while True:	
+		t = requests.get("https://5sim.net/v1/guest/products/russia/any").text
+		t = json.loads(t.replace("'",'"'))
+		google = t["google"]['Qty']
+		if google != google_last:
+			bot.send_message(message.chat.id, "Есть " + str(google[i]) +  " шт.")
+			google_last = google
+		else:
+			google_last = google
+		time.sleep(10)
+
+bot.polling(none_stop=True)
