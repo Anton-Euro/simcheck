dc2a0bfc8e938feda1842f973296eaf76e406248
Anton-Euro <62991246+Anton-Euro@users.noreply.github.com>
2021-02-10 20:39:18 +0300

    add

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
-# https://5sim.net/v1/guest/products/ghana/virtual2
-# https://5sim.net/v1/guest/products/russia/any
+# https://vak-sms.com/api/getCountNumber/?apiKey=f581ed49e50b4608946cee8e9c17e556&service=gl&country=kz&price
+# https://vak-sms.com/api/getCountNumber/?apiKey=f581ed49e50b4608946cee8e9c17e556&service=gl&country=id&price
+# https://vak-sms.com/api/getCountNumber/?apiKey=f581ed49e50b4608946cee8e9c17e556&service=gl&country=ru&price
 
 import config
 import telebot
@@ -14,14 +15,13 @@ def start(message):
 	bot.send_message(message.chat.id, "Поиск начался")
 	google_last = 0
 	while True:	
-		t = requests.get("https://5sim.net/v1/guest/products/ghana/virtual2").text
-		t = json.loads(t.replace("'",'"'))
-		google = t["google"]['Qty']
+		t = requests.get("https://vak-sms.com/api/getCountNumber/?apiKey=f581ed49e50b4608946cee8e9c17e556&service=gl&country=kz&price").text
+		google = t.split()[1][:-1]
 		if google != google_last:
 			bot.send_message(message.chat.id, "Есть " + str(google) +  " шт.")
 			google_last = google
 		else:
 			google_last = google
-		time.sleep(10)
+		time.sleep(20)
 
 bot.polling(none_stop=True)
